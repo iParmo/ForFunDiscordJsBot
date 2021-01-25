56cc5a37b772f1ca1c098c3199381de533e53f0e
Magnus Parmo <73383707+iParmo@users.noreply.github.com>
2021-01-25 22:29:48 +0100

    Updated
    Added a level system along with an unfinished database to store the level

--- JSBot/src/bot.js
+++ JSBot/src/bot.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+const sqlite = require('sqlite3').verbose();
+
+let db = new sqlite.Database('./db/rank.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to rank database.')
+});
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -17,6 +26,7 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -29,6 +39,27 @@ client.on('message', message => {
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+var xp = 0
+var neededXp = 100
+var level = 1
+
+client.on('message', message => {
+	xp += 10
+
+	if (xp === neededXp) {
+		xp = 0
+		level++
+		neededXp * 1.5
+		const embed = new Discord.MessageEmbed()
+			.setTitle('LEVEL UP!')
+			.setDescription(`You just leveled up! You're now level ${level}`)
+			.setThumbnail(message.author.avatarURL)
+			.setFooter(message.author.tag)
+			.setColor('BLUE')
+		message.channel.send(message.author, embed)
 	}
 });
 
